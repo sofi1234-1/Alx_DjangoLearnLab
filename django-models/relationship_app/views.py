b087fdb44192a84a7617b900ae5bbeca45ca6769
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 16:13:23 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from relationship_app.models import Book
+from django.views.generic import DetailView
+from relationship_app.models import Book, Library  # Import both Book and Library models
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-from django.views.generic import DetailView
-from relationship_app.models import Library
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
