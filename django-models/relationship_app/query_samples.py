063c4d52038628360ec3ace8ec01fc697f20f7d8
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 16:21:15 +0300

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -12,7 +12,7 @@ def query_books_in_library(library_name):
 
 def query_librarian_for_library(library_name):
     librarian = Library.objects.get(name=library_name)
-    librarian = library.librarian
+    library = library.librarian
     return librarian
 
 # Example usage:
