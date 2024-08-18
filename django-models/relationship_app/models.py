4fdd0e43d4f528e94c1aa1cd805539acbeb6d587
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 17:15:27 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -37,3 +37,16 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    # Other fields for your Book model
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
