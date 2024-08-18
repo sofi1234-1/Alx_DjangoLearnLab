8547e23416c37533d7ce117dd90e219c001321a7
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 20:29:12 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -96,3 +96,28 @@ def librarian_view(request):
 def member_view(request):
     # Your 'Member' view logic here
     return render(request, 'member_template.html')
+# views.py
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'role_based_views/adminview.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'role_based_views/librarianview.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'role_based_views/memberview.html')
