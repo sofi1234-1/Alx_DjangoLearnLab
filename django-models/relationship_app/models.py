7140349ef836ad4320701c077bdf7e3911aae41c
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 20:10:04 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -50,3 +50,21 @@ class Book(models.Model):
             ('can_change_book', 'Can change book'),
             ('can_delete_book', 'Can delete book'),
         ]
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+# Automatically create UserProfile when a new user is registered
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+models.signals.post_save.connect(create_user_profile, sender=User)
