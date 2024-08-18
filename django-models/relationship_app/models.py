1008f3a7f3ac895623bdc519da99473a164bf6d4
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-18 17:00:40 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -26,3 +26,14 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
