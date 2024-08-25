d65d744f2829b3273197810784176aff34690d71
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-25 15:11:21 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 SECURE_SSL_REDIRECT
+SECURE_HSTS_SECONDS
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
