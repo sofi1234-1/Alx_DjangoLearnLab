714616d421de35b4623a9575f1c64556dee18390
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-25 10:01:24 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,4 +7,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def str(self):
+        return self.username
    
