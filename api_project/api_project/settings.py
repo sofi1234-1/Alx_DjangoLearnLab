01751b1ac76495da27b432f71d2932003b0830d0
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-31 10:54:03 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -127,7 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthenticated', 
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthenticated',
     ],
 }
 
