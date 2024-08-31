9faf8c29216be8b9ced219d7b5abe8041a5e38e7
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-08-31 10:40:56 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+   
