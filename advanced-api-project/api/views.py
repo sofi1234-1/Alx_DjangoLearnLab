4de1757fc802d29fd42dc59e66b7a1c2fe48b734
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-08 14:49:02 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -15,3 +15,29 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
 CreateView
 UpdateView 
 DeleteView
+# views.py
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    
+    # Define filter fields
+    filterset_fields = {
+        'title': ['exact', 'icontains'],
+        'author': ['exact', 'icontains'],
+        'published_date': ['exact', 'year__gt', 'year__lt'],
+    }
+    
+    # Define search fields
+    search_fields = ['title', 'author']
+    
+    # Define ordering fields
+    ordering_fields = ['title', 'published_date']
+    ordering = ['title']  # Default ordering
+
