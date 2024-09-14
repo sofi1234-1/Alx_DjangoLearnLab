4f25379509001849a2860a676320cb37fb13df46
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-14 17:19:38 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -152,6 +152,17 @@ class CommentUpdateView(LoginRequiredMixin, UpdateView):
 class CommentDeleteView(LoginRequiredMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_delete_confirm.html'
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import SearchForm
+
+def search(request):
+    form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
+        return render(request, 'blog/search_results.html', {'posts': posts})
+    return render(request, 'blog/search.html', {'form': form})
 
     def get_queryset(self):
         return Comment.objects.filter(author=self.request.user)
