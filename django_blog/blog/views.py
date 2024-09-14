55e88e8711b0497e697d41e1e3b0363c8dd8d8c0
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-14 14:15:31 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+    return render(request, 'profile.html', {'form': form})
