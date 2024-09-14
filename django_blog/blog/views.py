16312ccc330d23d36fa79d01d27d56ee1fffb452
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-14 14:31:20 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+    return render(request, 'blog/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = CustomUserCreationForm(instance=request.user)
-    return render(request, 'profile.html', {'form': form})
+        user = request.user
+        user.email = request.POST['email']
+        user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html', {'user': request.user})
