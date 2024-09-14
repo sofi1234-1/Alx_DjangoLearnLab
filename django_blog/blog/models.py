4da823a2660038629b71de452c3baf209904c18b
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-14 17:02:51 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -26,3 +26,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+# blog/models.py
+
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author} on {self.post}'
+
+# Optional: any other necessary imports
