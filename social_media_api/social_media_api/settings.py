9af599ea0defb08f6e9f7c65f097d2c97c57b74f
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-22 11:45:04 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4iyg+seb5a@+cj5w2ozm0tc-i*37t5m77p333g1&6#f5jo_rj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
