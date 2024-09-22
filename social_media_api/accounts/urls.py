0cbd971373dcb946705834a0427c912c128353a0
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-09-22 11:29:54 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,4 +4,6 @@ from .views import RegistrationView, LoginView
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
 ]
